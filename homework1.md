17ab1667ee98c7750261225a788cc5e0e69096ae
Abkaplesh02 <abkaplesh02@gmail.com>
2025-05-13 23:49:07 +0530

    Pagination

--- homework1.md
+++ homework1.md
@@ -156,3 +156,15 @@ Ques1=>What happen if we send request and dont send any response in it
 
 -Thought process -POST vs GET
 
+-Pagination:::
+
+/feed?page=1&limit=10=>first 10 users 1-10   .skip(0) & limit(10)
+
+/feed?page=2&limit=10=>11-20   .skip(10) & limit(10)
+
+/feed?page=3&limit=10=>20-30   .skip(20) & limit(10)
+
+.skip()  &  .limit();
+
+skip=(page-1)*limit
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -54,6 +54,7 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
     }
 })
 
+
 // feed APi
 
 userRouter.get("/feed",userAuth,async(req,res)=>{
@@ -64,8 +65,13 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
         // rejected connections
         //ignored connections
         // already sent the connection request
+        const page=parseInt(req.query.page) || 1;
+        let limit=parseInt(req.query.limit)||10;
+        limit=limit>50 ? 50 :limit;
+        const skip=(page-1)*limit;
 
         const loggedInUser=req.user;
+        
 
         // Find all connection request
       const connectionRequest=await ConnectionRequest.find({
@@ -84,7 +90,7 @@ userRouter.get("/feed",userAuth,async(req,res)=>{
             {_id: { $nin: Array.from(hideUsersFromFeed)}},
             {_id: { $ne: loggedInUser._id}},
         ]
-      }).select(USER_SAFE_DATA);
+      }).select(USER_SAFE_DATA).skip(skip).limit(limit);
 
 
       res.send(users);
